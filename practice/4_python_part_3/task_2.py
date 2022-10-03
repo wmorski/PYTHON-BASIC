ae0a4c89a1afab6c7c23efe67bd2550dde57dcda
wmorski <wmorski@griddynamics.com>
2022-10-03 15:10:31 +0200

    complete tasks part 3

--- practice/4_python_part_3/task_2.py
+++ practice/4_python_part_3/task_2.py
@@ -8,14 +8,61 @@ Examples:
      10.0
      >>> math_calculate('ceil', 10.7)
      11
+     >>> math_calculate('unknown_function', 10, 10)
+     10
+     >>> math_calculate('sqrt', 4)
+     2.0
 """
 import math
+import pytest
+
+
+class OperationNotFoundException(AttributeError):
+    pass
 
 
 def math_calculate(function: str, *args):
-    ...
+    try:
+        if len(args) > 2:
+            print('Restriction: math function could take 1 or 2 arguments')
+            return None
+
+        func = getattr(math, function)
+
+    except AttributeError:
+        raise OperationNotFoundException("Given operation does not exist")
+
+    else:
+        return func(*args)
 
 
 """
 Write tests for math_calculate function
 """
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    (['log', 1024, 2, 2], 10)
+])
+def test_math_calculate_more_args(test_input, expected, capfd):
+    assert math_calculate(test_input[0], test_input[1], test_input[2], test_input[3]) is None
+    out, err = capfd.readouterr()
+    assert out == 'Restriction: math function could take 1 or 2 arguments\n'
+
+
+@pytest.mark.parametrize('test_input, expected', [(['log', 1024, 2], 10)])
+def test_math_calculate_2_args(test_input, expected):
+    result = math_calculate(test_input[0], test_input[1], test_input[2])
+    assert result == expected
+
+
+@pytest.mark.parametrize('test_input, expected', [(['ceil', 10.7], 11), (['sqrt', 4], 2)])
+def test_math_calculate_1_arg(test_input, expected):
+    result = math_calculate(test_input[0], test_input[1])
+    assert result == expected
+
+
+def test_math_calculate_exception():
+    with pytest.raises(OperationNotFoundException) as e:
+        math_calculate('unknown_function', 10, 10)
+    assert "Given operation does not exist" in str(e.value)
--- practice/4_python_part_3/task_5.py
+++ practice/4_python_part_3/task_5.py
@@ -2,14 +2,17 @@
 Write a function that makes a request to some url
 using urllib. Return status code and decoded response data in utf-8
 Examples:
-     >>> make_request('https://www.google.com')
+     >>> make_request('http://www.google.com')
      200, 'response data'
 """
 from typing import Tuple
+from urllib.request import urlopen
+from unittest.mock import Mock
 
 
 def make_request(url: str) -> Tuple[int, str]:
-    ...
+    with urlopen(url) as response:
+        return response.status, response.read().decode('utf-8')
 
 
 """
@@ -24,3 +27,15 @@ Example:
     >>> m.method2()
     b'some text'
 """
+
+
+def test_make_request():  # run in iterm using pytest task_5.py
+    status, data = make_request('https://api.myip.com/')
+
+    m = Mock()
+    m.status_code.return_value = 200
+    m.response_data.return_value = '{"ip":"193.109.103.128","country":"United Kingdom","cc":"GB"}'
+
+    assert m.status_code() == status
+    assert m.response_data() == data
+
