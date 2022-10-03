4ba47a66752680be48816113dc9fd9c4ee86913f
wmorski <wmorski@griddynamics.com>
2022-10-03 14:31:42 +0200

    complete tasks testing

--- practice/3_python_testing/test_read_write.py
+++ practice/3_python_testing/test_read_write.py
@@ -1,6 +1,14 @@
 """
-Write tests for 2_python_part_2/task_read_write.py task.
+Write tests for python_part_2/task_read_write.py task.
 To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
 """
+from practice.python_part_2.task_read_write import read_write
+
+
+def test_read_write_task(tmpdir):
+    path_to_files = '/Users/wmorski/PycharmProjects/PYTHON-BASIC/practice/python_part_2/files'
+    result = tmpdir.join('test_result.txt')
+    read_write(result, path_to_files)
+    assert result.read() == '80, 37, 15, 14, 99, 99, 59, 90, 69, 39, 67, 91, 74, 40, 32, 82, 48, 1, 95, 66'
--- practice/3_python_testing/test_task_parametrize.py
+++ practice/3_python_testing/test_task_parametrize.py
@@ -2,13 +2,12 @@
 Write a parametrized test for two functions.
 The functions are used to find a number by ordinal in the Fibonacci sequence.
 One of them has a bug.
-
 Fibonacci sequence: https://en.wikipedia.org/wiki/Fibonacci_number
-
 Task:
  1. Write a test with @pytest.mark.parametrize decorator.
  2. Find the buggy function and fix it.
 """
+import pytest
 
 
 def fibonacci_1(n):
@@ -20,6 +19,19 @@ def fibonacci_1(n):
 
 def fibonacci_2(n):
     fibo = [0, 1]
-    for i in range(1, n+1):
-        fibo.append(fibo[i-1] + fibo[i-2])
+    for i in range(2, n + 1):
+        fibo.append(fibo[i - 1] + fibo[i - 2])
     return fibo[n]
+
+
+parameters = [(1, 1), (2, 1), (3, 2), (4, 3), (5, 5), (6, 8), (7, 13)]
+
+
+@pytest.mark.parametrize('test_input, expected', parameters)
+def test_fibonacci_1(test_input, expected):
+    assert fibonacci_1(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input, expected', parameters)
+def test_fibonacci_2(test_input, expected):
+    assert fibonacci_2(test_input) == expected
